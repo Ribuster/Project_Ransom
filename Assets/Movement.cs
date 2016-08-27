e6586ad75ba58e33f6a96d5e3d7c80e924742099
Adam Paquette <GamDevEmail@gmail.com>
2016-08-26 21:25:33 -0700

    Adding default files to repository

---
+++ Assets/Movement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float xStart;
+	public float yStart;
+	public float xSpeed;
+	public float ySpeed;
+	public Rigidbody2D rb;
+	private double xPrevious;
+	private double yPrevious;
+	public ForceMode2D mode;
+	public Movement character;
+
+	// Use this for initialization
+	void Start () {
+		mode = ForceMode2D.Force;
+		rb = GetComponent<Rigidbody2D>();
+		//character = GetComponent<Movement>();
+		GetComponent<Rigidbody2D>().position = new Vector3(xStart, yStart);
+
+	}
+
+	// Update is called once per frame
+	public void Update () {
+	}
+
+	// Update is called every significant frame
+	public void FixedUpdate () {
+		// make this a bounding box
+		xPrevious = rb.position.x;
+		yPrevious = rb.position.y;
+		//character.MoveUp();
+		//character.MoveRight();
+	}
+
+	public void MoveUp () {
+		Vector2 vector = new Vector2(0, ySpeed);
+		rb.AddForce(vector, mode);
+	}
+	public void MoveDown () {
+		Vector2 vector = new Vector2(0, -ySpeed);
+		rb.AddForce(vector, mode);
+	}
+
+	public void MoveRight () {
+		Vector2 vector = new Vector2(xSpeed, 0);
+		rb.AddForce(vector, mode);
+	}
+
+	public void MoveLeft () {
+		Vector2 vector = new Vector2(-xSpeed, 0);
+		rb.AddForce(vector, mode);
+	}
+	public void Stop () {
+		//rb.
+	}
+}
---
+++ Assets/playerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+	private Movement movement;
+	private Rigidbody2D rb;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		movement = GetComponent<Movement>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		if (Input.GetButton("Right") && rb.velocity.x <= speed) {
+			movement.MoveRight();
+		}
+		if (Input.GetButton("Left") && rb.velocity.x >= -speed) {
+			movement.MoveLeft();
+		}
+		if (Input.GetButton("Up") && rb.velocity.y <= speed) {
+			movement.MoveUp();
+	  }
+	  if (Input.GetButton("Down") && rb.velocity.y >= -speed) {
+		  movement.MoveDown();
+		}
+  }
+}
